f932d2e9c09621821052ced88ede57e1be51d128
zhouyantong <zhouyantong@hichao.con>
2014-12-21 17:32:44 +0800

    Initial Commit

---
+++ paowu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  paowu
+//
+//  Created by zhouyantong on 14/12/21.
+//  Copyright (c) 2014年 zhouyantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ paowu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  paowu
+//
+//  Created by zhouyantong on 14/12/21.
+//  Copyright (c) 2014年 zhouyantong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
